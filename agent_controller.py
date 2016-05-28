f072b0468e86ce17811b89b414e05229f9d8d9a2
syundo0730 <syundo0730@gmail.com>
2016-05-28 12:23:18 +0900

    復数種類のアクションを表示する

---
+++ agent_controller.py
@@ -0,0 +1,55 @@
+from cnn_dqn_agent import CnnDqnAgent
+import argparse
+
+parser = argparse.ArgumentParser(description='ml-agent-for-deresta')
+parser.add_argument('--gpu', '-g', default=-1, type=int, help='GPU ID (negative value indicates CPU)')
+args = parser.parse_args()
+
+
+class AgentController:
+    def __init__(self):
+        self.agent = CnnDqnAgent()
+        self.agent_initialized = False
+        self.cycle_counter = 0
+        self.log_file = 'reward.log'
+        self.reward_sum = 0
+        # press, up, down, left, right, none
+        self.commands = ["none", "none", "none", "none", "none"]
+
+    def get_commands(self):
+        return self.commands
+
+    def set_commands_from_action(self, action):
+        command_candidate = ["press", "up", "right", "left", "none"]
+        self.commands = [command_candidate[a] for a in action]
+
+    def update(self, message):
+        image = message["image"]
+        pad_states = message["pad_states"]
+        end_episode = message['end_episode']
+        observation = {"image": image, "pad_states": pad_states}
+        reward = message['reward']
+
+        if not self.agent_initialized:
+            self.agent_initialized = True
+            print ("initializing agent......")
+            self.agent.agent_init(use_gpu=args.gpu, pad_states_dim=len(pad_states))
+
+            action = self.agent.agent_start(observation)
+            self.set_commands_from_action(action)
+            with open(self.log_file, 'w') as the_file:
+                the_file.write('cycle, episode_reward_sum \n')
+        else:
+            self.cycle_counter += 1
+            self.reward_sum += reward
+
+            if end_episode:
+                self.agent.agent_end(reward)
+                with open(self.log_file, 'a') as the_file:
+                    the_file.write(str(self.cycle_counter) +
+                                   ',' + str(self.reward_sum) + '\n')
+                self.reward_sum = 0
+            else:
+                action, eps, q_now, obs_array = self.agent.agent_step(reward, observation)
+                self.set_commands_from_action(action)
+                self.agent.agent_step_update(reward, action, eps, q_now, obs_array)
---
+++ game_viewer.py
@@ -0,0 +1,82 @@
+from deresta_score_service import ScoreEvaluator, NotesLoader
+import cv2
+import numpy as np
+
+
+class GameViewer():
+    def __init__(self):
+        self.capture = None
+        self.width = 0
+        self.height = 0
+        self.enabled = True
+        self.window_name = "CinderellaStage"
+        self.frame_num = 0
+        cv2.namedWindow(self.window_name, cv2.WINDOW_NORMAL)
+
+    def __del__(self):
+        self.capture.release()
+        cv2.destroyAllWindows()
+
+    def init(self):
+        if self.capture is not None and self.capture.isOpened():
+            self.capture.release()
+        self.capture = cv2.VideoCapture('anzu.mp4')
+        self.width = self.capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.frame_num = self.capture.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    def read(self):
+        if self.capture is not None and self.capture.isOpened():
+            ret, frame = self.capture.read()
+            if not ret:
+                return None, 0, True
+            time = self.capture.get(cv2.CAP_PROP_POS_MSEC)
+            frame_pos = self.capture.get(cv2.CAP_PROP_POS_FRAMES)
+            return frame, time, (frame_pos == self.frame_num-1)
+        else:
+            return None, 0, True
+
+    def show(self, frame, command, score):
+        self.draw_action(frame, command, score)
+        cv2.imshow(self.window_name, frame)
+
+    def draw_action(self, frame, commands, scores):
+        w_center = self.width * 0.5
+        interval = self.width / 6.5
+        pos_x_1 = int(w_center - interval * 2)
+        pos_x_2 = int(w_center - interval)
+        pos_x_3 = int(w_center)
+        pos_x_4 = int(w_center + interval)
+        pos_x_5 = int(w_center + interval * 2)
+        pos_y = int(self.height - self.height / 6.0)
+
+        self.draw_command(frame, (pos_x_1, pos_y), commands[0], scores[0])
+        self.draw_command(frame, (pos_x_2, pos_y), commands[1], scores[1])
+        self.draw_command(frame, (pos_x_3, pos_y), commands[2], scores[2])
+        self.draw_command(frame, (pos_x_4, pos_y), commands[3], scores[3])
+        self.draw_command(frame, (pos_x_5, pos_y), commands[4], scores[4])
+
+    def draw_command(self, frame, pos, command, score):
+        # BGR
+        blue = (255, 0, 0)
+        red = (0, 0, 255)
+        yellow = (0, 255, 255)
+        black = (0, 0, 0)
+        if command == "press":
+            cv2.circle(frame, pos, 40, yellow, 3, 4)
+        if command == "up":
+            cv2.circle(frame, pos, 40, red, 3, 4)
+        if command == "down":
+            cv2.circle(frame, pos, 40, blue, 3, 4)
+        if command == "left":
+            cv2.fillConvexPoly(
+                frame,
+                np.array([[pos[0]-30, pos[1]], [pos[0]+30, pos[1]+30], [pos[0]+30, pos[1]-30]]),
+                yellow)
+        if command == "right":
+            cv2.fillConvexPoly(
+                frame,
+                np.array([[pos[0]+30, pos[1]], [pos[0]-30, pos[1]-30], [pos[0]-30, pos[1]+30]]),
+                yellow)
+        cv2.putText(frame, str(score), (pos[0], pos[1] - 40), cv2.FONT_HERSHEY_PLAIN, 2, black, 2)
+
